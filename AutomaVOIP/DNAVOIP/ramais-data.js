5cf3a56dba435e646334b0d4bc49139815d5ea4f
Talisson de Jesus Paixao <talisson.paixao@dna.local>
2025-06-20 17:24:22 -0300

    atualizações layout

---
+++ AutomaVOIP/DNAVOIP/ramais-data.js
@@ -0,0 +1,85 @@
+const setores = [
+  {
+    setor: "Biologia Molecular",
+    ramais: [
+      { numero: "7119", nome: "Coordenação Paula/Layla" },
+      { numero: "7103", nome: "Assessoria médica-científica" },
+      { numero: "7154", nome: "Biologia Molecular (Rastreio)" }
+    ]
+  },
+  {
+    setor: "TI",
+    ramais: [
+      { numero: "7115", nome: "Jardel" },
+      { numero: "7116", nome: "Rafael" },
+      { numero: "7117", nome: "Talisson" },
+      { numero: "7144", nome: "Emerson" },
+      { numero: "7114", nome: "Danillo" }
+    ]
+  },
+  {
+    setor: "ARACAJU",
+    ramais: [
+      { numero: "20", nome: "Call Center" },
+      { numero: "23", nome: "ADM/Faturamento (Julio)" },
+      { numero: "24", nome: "Consultório 01 (Dr Emerson)" },
+      { numero: "25", nome: "Consultório 02 (Dra Julia/Elaine)" },
+      { numero: "26", nome: "Gerência (Luciana)" },
+      { numero: "28", nome: "Recepção (Marcela)" },
+      { numero: "29", nome: "Recepção (Geise)" },
+      { numero: "30", nome: "Recepção (Wilna)" },
+      { numero: "31", nome: "Área Técnica" }
+    ]
+  },
+  {
+    setor: "Financeiro",
+    ramais: [
+      { numero: "7137", nome: "Rebeca" },
+      { numero: "7133", nome: "Rafaela" }
+    ]
+  },
+  {
+    setor: "PITUBA - Consultórios e Técnicos",
+    ramais: [
+      { numero: "7407", nome: "Assessoria médica" },
+      { numero: "7419", nome: "Bioquímica" },
+      { numero: "7418", nome: "Hematologia" },
+      { numero: "7421", nome: "Hormônio / Bancada" },
+      { numero: "7424", nome: "Uroanálise" },
+      { numero: "7425", nome: "Clinin" },
+      { numero: "7426", nome: "Clinin" },
+      { numero: "7420", nome: "Gerência / Área Técnica (Isabel)" },
+      { numero: "7411", nome: "Microbiologia" },
+      { numero: "7402", nome: "Portaria" },
+      { numero: "7400", nome: "Recepção" },
+      { numero: "7401", nome: "Recepção" },
+      { numero: "7412", nome: "CDA" },
+      { numero: "7409", nome: "Triagem/Pezinho (Lorena)" },
+      { numero: "7410", nome: "Patologia 2" },
+      { numero: "7550", nome: "Patologia - Coordenação" }
+    ]
+  },
+  {
+    setor: "RH",
+    ramais: [
+      { numero: "7145", nome: "Fernanda" },
+      { numero: "SN", nome: "Carla" }
+    ]
+  },
+  {
+    setor: "PITUBA - Amazonas / Telefonia",
+    ramais: [
+      { numero: "7422", nome: "Edna" },
+      { numero: "7404", nome: "Consultório Médico 01" },
+      { numero: "7405", nome: "Consultório Médico 02" },
+      { numero: "7516", nome: "Vacinas" },
+      { numero: "7540", nome: "Cassiana (Qualidade)" },
+      { numero: "7541", nome: "Isabele (Qualidade)" },
+      { numero: "1071", nome: "Coordenação (Coleta Domiciliar)" },
+      { numero: "1043", nome: "Coleta Domiciliar" },
+      { numero: "1037", nome: "Coleta Domiciliar" },
+      { numero: "1072", nome: "Carine Siqueira" },
+      { numero: "SAC 1 a 14", nome: "Ingenium" }
+    ]
+  }
+];
